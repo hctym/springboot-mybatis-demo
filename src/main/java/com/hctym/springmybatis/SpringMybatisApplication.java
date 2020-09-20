34161162d43c6240c4a964ce59e69b350969f04a
xulinchuang <972927683@qq.com>
2020-09-20 22:02:47 +0800

    ''

--- src/main/java/com/hctym/springmybatis/SpringMybatisApplication.java
+++ src/main/java/com/hctym/springmybatis/SpringMybatisApplication.java
@@ -1,19 +1,24 @@
 package com.hctym.springmybatis;
 
+import io.swagger.annotations.Api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @RestController
 @SpringBootApplication
 @MapperScan("com.hctym.springmybatis.mapper")
+@EnableOpenApi
+@Api(tags = "startDemo")
 public class SpringMybatisApplication extends SpringBootServletInitializer{
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String hello(){
 		return "hellp springboot!";
 	}
--- src/main/java/com/hctym/springmybatis/controller/UserController.java
+++ src/main/java/com/hctym/springmybatis/controller/UserController.java
@@ -2,24 +2,29 @@ package com.hctym.springmybatis.controller;
 
 import com.hctym.springmybatis.model.User;
 import com.hctym.springmybatis.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
+@Api(tags = "用户管理")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "id/{id}",method = RequestMethod.GET)
+    @ApiOperation(value = "查询用户详情")
+    @GetMapping(value = "id/{id}")
     @ResponseBody
     public User queryUserById(@PathVariable int id){
         return userService.queryUserById(id);
     }
 
-    @RequestMapping(value="/add", method = RequestMethod.POST)
+    @ApiOperation(value = "添加用户")
+    @PostMapping(value="/add")
     @ResponseBody
     public int addUser(User user){
         return userService.addUser(user);
@@ -27,8 +32,9 @@ public class UserController {
 
 
 
-    @RequestMapping("test/{id}")
+    @GetMapping("test/{id}")
     @ResponseBody
+    @ApiOperation(value = "test用户")
     public String demo(@PathVariable int id){
         return "hello mock "+id;
     }
